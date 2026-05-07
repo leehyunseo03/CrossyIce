71e92503aaeaa8174c73d653f76e899d3b94fbeb
leehyunseo03 <falcon10179@gmail.com>
2026-05-07 21:31:37 +0900

    feat: add player movement

--- CrossyIce/Session.fs
+++ CrossyIce/Session.fs
@@ -9,18 +9,24 @@ type Session(stageDefinition: StageDefinition) =
     let isKeyPressed key : bool =
         Raylib.IsKeyPressed(key)
 
-    let updatePlayerDirection () =
+    let updatePlayerMovement () =
+        let playerPos = player.getPosition
+
         if isKeyPressed KeyboardKey.W then
             player.setDirection Back
+            player.setPosition {playerPos with Y = playerPos.Y - 1}
         elif isKeyPressed KeyboardKey.S then
             player.setDirection Front
+            player.setPosition {playerPos with Y = playerPos.Y + 1}
         elif isKeyPressed KeyboardKey.A then
             player.setDirection Left
+            player.setPosition {playerPos with X = playerPos.X - 1}
         elif isKeyPressed KeyboardKey.D then
             player.setDirection Right
+            player.setPosition {playerPos with X = playerPos.X + 1}
 
     member _.StageMap = stageMap
     member _.Player = player
 
     member _.Update() =
-        updatePlayerDirection ()
+        updatePlayerMovement ()
