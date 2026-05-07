209448bafb9018ccaee591cb9aa070db95494647
leehyunseo03 <falcon10179@gmail.com>
2026-05-07 21:44:45 +0900

    feat: add player collision

--- CrossyIce/Session.fs
+++ CrossyIce/Session.fs
@@ -9,21 +9,39 @@ type Session(stageDefinition: StageDefinition) =
     let isKeyPressed key : bool =
         Raylib.IsKeyPressed(key)
 
+    let checkCollision (point: GridPoint) =
+        let cellType = stageMap.CellAt point 
+        match cellType with
+        | SolidWall -> true
+        | FragileWall -> true
+        | _ -> false
+
     let updatePlayerMovement () =
         let playerPos = player.getPosition
 
-        if isKeyPressed KeyboardKey.W then
-            player.setDirection Back
-            player.setPosition {playerPos with Y = playerPos.Y - 1}
-        elif isKeyPressed KeyboardKey.S then
-            player.setDirection Front
-            player.setPosition {playerPos with Y = playerPos.Y + 1}
-        elif isKeyPressed KeyboardKey.A then
-            player.setDirection Left
-            player.setPosition {playerPos with X = playerPos.X - 1}
-        elif isKeyPressed KeyboardKey.D then
-            player.setDirection Right
-            player.setPosition {playerPos with X = playerPos.X + 1}
+        let newPos = 
+            if isKeyPressed KeyboardKey.W then
+                player.setDirection Back
+                Some {playerPos with Y = playerPos.Y - 1}
+            elif isKeyPressed KeyboardKey.S then
+                player.setDirection Front
+                Some {playerPos with Y = playerPos.Y + 1}
+            elif isKeyPressed KeyboardKey.A then
+                player.setDirection Left
+                Some {playerPos with X = playerPos.X - 1}
+            elif isKeyPressed KeyboardKey.D then
+                player.setDirection Right
+                Some {playerPos with X = playerPos.X + 1}
+            else
+                None
+        
+        match newPos with
+        | Some pos -> 
+            if checkCollision pos then
+                ()
+            else 
+                player.setPosition pos
+        | None -> ()
 
     member _.StageMap = stageMap
     member _.Player = player
